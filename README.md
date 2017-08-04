140a6bb4b8180a6e42e5249b16cdea94795c7141
Chandan Maruthi <chandan.maruthi@gmail.com>
2017-08-03 22:10:30 -0700

    updated urls

--- README.md
+++ README.md
@@ -6,6 +6,11 @@ Welcome to Contacts App
 
 * Build a app that resembles Mac contacts
 
+### What does it do  ###
+*  Able to import Google address book from csv
+*  Load data into db/Elastic search and make it available for a user to access
+*  Fast response with Ajax/Elastic that prevent screen refresh , trying to keep close to the Mac contacts experience
+
 ### Design Considerations  ###
 
 *  Data Model design [Mysql] - Completed
@@ -21,6 +26,10 @@ Welcome to Contacts App
 * Possible , however very little time had been spent on packaging, total time spent 2 days
 * Constraints, Learning curve involved for 1 technology [Java Spring]
 
+### What has not been done  ###
+* Security
+* Clean install from git
+* Export data, edit data
 
 ###Things that could go wrong###
 * Will update shortly
--- curious/curiousWorkbench/urls.py
+++ curious/curiousWorkbench/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import include
 from django.conf.urls import url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-
+import ajax
 dir_path = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(dir_path)
 
@@ -17,10 +17,10 @@ import ajax
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^ajax/add/$', 'ajax.add_todo'),
-    url(r'^ajax/more/$', 'ajax.more_todo'),
-    url(r'^ajax/search/$', 'ajax.searchContacts'),
-    url(r'^ajax/details/$', 'ajax.details'),
+    url(r'^ajax/add/$', 'add_todo'),
+    url(r'^ajax/more/$', 'more_todo'),
+    url(r'^ajax/search/$', 'searchContacts'),
+    url(r'^ajax/details/$', 'details'),
     url(r'^login/', views.user_login, name="user_login"),
     url(r'^handleActions/', views.handleActions, name='handleActions'),
 
